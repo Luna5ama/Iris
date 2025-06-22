d2681802595ff7c9a89ab5d518ee82b184738118
Luna <62033805+Luna5ama@users.noreply.github.com>
2025-06-22 07:05:00 -0400

    DH shadow viewport fix

--- common/src/main/java/net/irisshaders/iris/pipeline/IrisRenderingPipeline.java
+++ common/src/main/java/net/irisshaders/iris/pipeline/IrisRenderingPipeline.java
@@ -1032,6 +1032,13 @@ public class IrisRenderingPipeline implements WorldRenderingPipeline, ShaderRend
 	}
 
 	@Override
+	public void setupShadowViewport() {
+		if (shadowRenderer != null) {
+			this.shadowRenderer.setupShadowViewport();
+		}
+	}
+
+	@Override
 	public void addDebugText(DebugScreenDisplayer messages) {
 		if (this.shadowRenderer != null) {
 			shadowRenderer.addDebugText(messages);
